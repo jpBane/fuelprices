1207ecef7789a38f66986f562618ebecc15bf16c
jpBane <54914492+jpBane@users.noreply.github.com>
2022-06-16 17:58:44 +0200

    request from only one station that works

--- fuelprices_region.py
+++ fuelprices_region.py
@@ -1,1 +1,29 @@
+# Load the required packges
+import requests
+from influxdb import InfluxDBClient
 
+# Define the variables
+client = InfluxDBClient(host='localhost', port=8086, database='fuelprices')
+
+response = requests.get("https://creativecommons.tankerkoenig.de/json/prices.php?ids=4429a7d9-fb2d-4c29-8cfe-2ca90323f9f8&apikey=00000000-0000-0000-0000-000000000002")
+
+if response
+  jsonResponse = response.json()
+  json_body = [
+    {
+      "measurement": "Region",
+      "tags": {
+        "station_id": "insert_id_here",
+        "station_name": "insert_name_here"
+      },
+      "fields": {
+        "e5": jsonResponse["prices"]["insert_id_here"]["e5"],
+        "diesel": jsonResponse["prices"]["insert_id_here"]["diesel"]
+      }
+    }
+  ]
+  print(json_body)
+else:
+  print('Request returned an error.')
+  
+client.write_points(json_body, time_precision='s')
